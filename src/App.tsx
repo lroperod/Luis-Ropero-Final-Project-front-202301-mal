0f02b5d13078a2021853bf90811da6f44cca5b2c
luis <luisrym@gmail.com>
2023-03-15 19:49:35 +0100

    add router provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import router from './router/router';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
