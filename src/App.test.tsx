067d7cc1f30de59ae030e3830deb849bcae5d66a
luis <luisrym@gmail.com>
2023-03-18 16:35:50 +0100

    refactor app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
-test("renders learn react link", () => {
-  render(<App />);
+test('renders learn react link', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   expect(true).toBe(true);
 });
