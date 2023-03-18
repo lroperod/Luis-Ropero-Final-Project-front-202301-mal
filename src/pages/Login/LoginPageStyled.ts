86cfec5bcc9493931b2b246e77a8c70743def8b9
luis <luisrym@gmail.com>
2023-03-18 18:49:38 +0100

    add new styles

--- src/pages/Login/LoginPageStyled.ts
+++ src/pages/Login/LoginPageStyled.ts
@@ -4,18 +4,21 @@ export const MainContainer = styled.main`
   display: flex;
   align-items: stretch;
   flex-direction: column;
+
   @media (min-width: 1024px) {
     flex-direction: row;
   }
 `;
 
 export const ImageLoginStyled = styled.div`
-  background-image: url('assets/images-login/login.jpg');
+  background-image: url('assets/images-login/login-mobile.webp');
   height: 600px;
   max-width: 100%;
   background-repeat: no-repeat;
   background-size: cover;
   @media (min-width: 1024px) {
     width: 50%;
+    height: 100vh;
+    background-image: url('assets/images-login/login-desktop.webp');
   }
 `;
