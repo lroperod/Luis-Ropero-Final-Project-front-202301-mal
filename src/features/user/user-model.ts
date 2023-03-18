a7712684467dd3c935764bf5668e7a2674352fa8
luis <luisrym@gmail.com>
2023-03-18 18:59:16 +0100

    create interface to user

---
+++ src/features/user/user-model.ts
@@ -0,0 +1,17 @@
+import { APIStatus } from '../../shared/models/api-status';
+
+export interface User {
+  email: string;
+  password: string;
+}
+export interface UserToken {
+  msg: string;
+  accessToken: string;
+}
+
+export interface UserStatus {
+  status: APIStatus;
+  loginStatus: 'loading' | 'success' | 'error' | 'idle';
+  loginMessage: string | undefined;
+}
+export type AuthUser = Pick<User, 'email' | 'password'>;
