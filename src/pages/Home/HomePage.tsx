b009c8384404b383207578fd531e452e56d72e14
luis <luisrym@gmail.com>
2023-03-21 12:34:08 +0100

    add styled to componente homepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ContinentCardList from '../../features/user/components/continent-cardlist/ContinentCardList';
 import { continents } from '../../shared/data/continent';
+import { HomeStyled } from './HomePageStyled';
 
 export const HomePage = () => {
-  return <ContinentCardList continents={continents}></ContinentCardList>;
+  return (
+    <HomeStyled>
+      <ContinentCardList continents={continents}></ContinentCardList>
+    </HomeStyled>
+  );
 };
