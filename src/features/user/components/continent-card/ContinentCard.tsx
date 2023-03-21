a15a2f49fcbfee95870aeeea377ef1ca1b32291a
luis <luisrym@gmail.com>
2023-03-21 07:32:06 +0100

    create continentcard component and his styledcomponent

---
+++ src/features/user/components/continent-card/ContinentCard.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { Continent } from '../../../../shared/data/continent';
+import {
+  ButtonContinentStyled,
+  CardContainer,
+  ImageContinentStyled,
+  TitleContinentStyled,
+} from './ContinentCardStyled';
+
+const ContinentCard: FC<Continent> = ({ imageURL, nameContinent, alt }) => {
+  return (
+    <CardContainer>
+      <TitleContinentStyled>{nameContinent}</TitleContinentStyled>
+      <ImageContinentStyled>
+        {<img src={imageURL} alt={alt}></img>}
+      </ImageContinentStyled>
+      <ButtonContinentStyled>Detail</ButtonContinentStyled>
+    </CardContainer>
+  );
+};
+
+export default ContinentCard;
---
+++ src/features/user/components/continent-card/ContinentCardStyled.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.article`
+  width: 280px;
+  margin: 10% auto;
+  user-select: none;
+  border-radius: 12px;
+  border: 1.5px solid #e4e4e4;
+  overflow: hidden;
+`;
+export const TitleContinentStyled = styled.h2`
+  font-family: 'poppins';
+  text-align: center;
+  color: grey;
+`;
+
+export const ImageContinentStyled = styled.div`
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+export const ButtonContinentStyled = styled.button`
+  width: 100%;
+  background-color: #2da4ff;
+  border: none;
+  box-sizing: border-box;
+  min-height: 2rem;
+  font-family: 'popins';
+  color: white;
+  font-size: 1rem;
+  font-weight: bold;
+`;
