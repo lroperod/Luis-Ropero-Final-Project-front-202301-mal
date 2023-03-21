ae639b729bd48f65b2c5d856bf204b26c9446332
luis <luisrym@gmail.com>
2023-03-21 07:23:53 +0100

    create a variable to mock ContinentCardList test

---
+++ src/mocks/data.ts
@@ -0,0 +1,19 @@
+import { Continent } from '../shared/data/continent';
+
+export const mockedContinents: Continent[] = [
+  {
+    nameContinent: 'Asia',
+    imageURL: 'assets/images-home/asia.webp',
+    alt: 'image-asia',
+  },
+  {
+    nameContinent: 'África',
+    imageURL: 'assets/images-home/africa.webp',
+    alt: 'image-africa',
+  },
+  {
+    nameContinent: 'América',
+    imageURL: 'assets/images-home/america.webp',
+    alt: 'image-america',
+  },
+];
