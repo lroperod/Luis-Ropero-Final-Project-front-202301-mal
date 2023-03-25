e783988e63442b18b3493d41327ff5f3d31114c3
luis <luisrym@gmail.com>
2023-03-25 11:28:18 +0100

    add new reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import userReducer from '../features/user/user-slice';
-
+import travelReducer from '../features/user/components/travels/travel-slice';
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    travels: travelReducer,
   },
 });
 
