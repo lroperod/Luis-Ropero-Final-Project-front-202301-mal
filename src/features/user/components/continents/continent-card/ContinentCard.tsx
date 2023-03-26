2ff607e03e24c16594215d42cf5ec72a8478de64
luis <luisrym@gmail.com>
2023-03-27 00:31:32 +0200

    add link to travel button

--- src/features/user/components/continents/continent-card/ContinentCard.tsx
+++ src/features/user/components/continents/continent-card/ContinentCard.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import { Continent } from '../../../../../shared/data/continent';
 import {
   ButtonContinentStyled,
@@ -14,7 +15,9 @@ const ContinentCard: FC<Continent> = ({ imageURL, nameContinent, alt }) => {
       <ImageContinentStyled>
         {<img src={imageURL} alt={alt}></img>}
       </ImageContinentStyled>
-      <ButtonContinentStyled>Detail</ButtonContinentStyled>
+      <Link to={'/travel'} style={{ textDecoration: 'none' }}>
+        <ButtonContinentStyled>Book now</ButtonContinentStyled>
+      </Link>
     </CardContainer>
   );
 };
