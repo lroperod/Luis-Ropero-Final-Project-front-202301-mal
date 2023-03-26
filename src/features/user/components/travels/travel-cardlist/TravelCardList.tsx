639b6cc448e0c6cd17d6f0627ce04f7ac2d9f1b2
luis <luisrym@gmail.com>
2023-03-27 01:20:06 +0200

    delete import link

--- src/features/user/components/travels/travel-cardlist/TravelCardList.tsx
+++ src/features/user/components/travels/travel-cardlist/TravelCardList.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../../../../app/hooks';
 
 import { APIStatus } from '../../../../../shared/models/api-status';
