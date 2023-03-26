16649aa78f7c0d53aeef631e206c977e20eaedda
luis <luisrym@gmail.com>
2023-03-27 00:28:36 +0200

    add new setupStore and rootReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,10 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  PreloadedState,
+  combineReducers,
+} from '@reduxjs/toolkit';
 import userReducer from '../features/user/user-slice';
 import travelReducer from '../features/user/components/travels/travel-slice';
 export const store = configureStore({
@@ -8,6 +14,18 @@ export const store = configureStore({
   },
 });
 
+const rootReducer = combineReducers({
+  user: userReducer,
+  travels: travelReducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
