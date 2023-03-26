6a9922d1044ac707c3979136f6e5e04e56bf122d
luis <luisrym@gmail.com>
2023-03-27 01:17:49 +0200

    add new styles

--- src/features/user/components/travels/travel-cardlist/TravelCardListStyled.ts
+++ src/features/user/components/travels/travel-cardlist/TravelCardListStyled.ts
@@ -6,3 +6,17 @@ export const TravelListContainer = styled.ul`
     width: 100%;
   }
 `;
+
+export const TravelStatusFeedBackLoading = styled.div`
+  color: green;
+  text-align: center;
+  font-size: 1.5rem;
+  margin-top: 3rem;
+`;
+
+export const TravelStatusFeedBackError = styled.div`
+  color: red;
+  text-align: center;
+  font-size: 1.5rem;
+  margin-top: 3rem;
+`;
