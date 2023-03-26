8b8d2aa5653f9c7ad5e102c2f3067ee4fa307e20
luis <luisrym@gmail.com>
2023-03-27 01:18:29 +0200

    delete styles

--- src/pages/MyTravels/TravelPageStyled.ts
+++ src/pages/MyTravels/TravelPageStyled.ts
@@ -1,10 +1,3 @@
 import styled from 'styled-components';
 
 export const TravelPageStyled = styled.article``;
-
-export const TravelStatusFeedBackError = styled.div`
-  color: red;
-  text-align: center;
-  font-size: 1.5rem;
-  margin-top: 3rem;
-`;
