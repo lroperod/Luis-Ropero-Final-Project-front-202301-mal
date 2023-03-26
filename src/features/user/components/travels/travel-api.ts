5f7870d9a10efab731f10562b1a8e1578eeb2fad
luis <luisrym@gmail.com>
2023-03-27 00:36:20 +0200

    refactor travel-api

--- src/features/user/components/travels/travel-api.ts
+++ src/features/user/components/travels/travel-api.ts
@@ -1,9 +1,9 @@
 export const getAllTravels = async () => {
   const response = await fetch(
-    `${process.env.REACT_APP_API_URL}api/vi/travel`,
+    `${process.env.REACT_APP_API_URL}/api/v1/travel`,
     {
       headers: {
-        Authorization: `Bearer ${sessionStorage.getItem('acessToken')}`,
+        Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
         'Content-Type': 'application/json',
       },
     },
