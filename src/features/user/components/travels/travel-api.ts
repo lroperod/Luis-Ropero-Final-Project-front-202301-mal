e91d631b98e9bfae546a1536413d765b7d0e784b
luis <luisrym@gmail.com>
2023-03-28 16:28:15 +0200

    create function to send a request HTTP post to api

--- src/features/user/components/travels/travel-api.ts
+++ src/features/user/components/travels/travel-api.ts
@@ -10,3 +10,17 @@ export const getAllTravels = async () => {
   );
   return response;
 };
+
+export const createNewTravel = async (newTravel: FormData) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/api/v1/travel`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+      },
+      body: newTravel,
+    },
+  );
+  return response;
+};
