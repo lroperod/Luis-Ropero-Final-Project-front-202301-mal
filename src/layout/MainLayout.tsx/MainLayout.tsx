da4b6f2117b39699afffcac925729a6ab7c84831
luis <luisrym@gmail.com>
2023-03-29 14:01:17 +0200

    create main layout component and its test

---
+++ src/layout/MainLayout.tsx/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from '../../shared/components/header/Header';
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+export default MainLayout;
