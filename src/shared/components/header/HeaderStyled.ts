cc65f00b88a81049160e42f019e0acbe489d5485
luis <luisrym@gmail.com>
2023-03-29 14:00:18 +0200

    create header component and its test

---
+++ src/shared/components/header/HeaderStyled.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const HeaderStyled = styled.header`
+  background: #2da4ff;
+  display: flex;
+  justify-content: space-between;
+
+  .logo {
+    padding: 1rem;
+  }
+  .logo img {
+    width: 30px;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+  }
+  nav a {
+    text-decoration: none;
+    font-size: 0.5rem;
+    color: white;
+    padding: 0.5rem;
+    text-align: center;
+  }
+  nav a p {
+    font-size: 10px;
+  }
+
+  nav img {
+    width: 25px;
+  }
+`;
