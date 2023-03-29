22d9a78e154db744c88da0d024578b21c467d6eb
luis <luisrym@gmail.com>
2023-03-29 13:55:44 +0200

    add new path with main layout and header component

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../layout/MainLayout.tsx/MainLayout';
 import CreateFormPage from '../pages/CreateForm/CreateFormPage';
 import { HomePage } from '../pages/Home/HomePage';
 import LoginPage from '../pages/Login/LoginPage';
@@ -8,18 +9,30 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <LoginPage />,
+    children: [
+      {
+        path: '',
+        element: <LoginPage />,
+      },
+    ],
   },
   {
-    path: '/home',
-    element: <HomePage />,
-  },
-  {
-    path: '/travel-form',
-    element: <CreateFormPage />,
-  },
-  {
-    path: '/travel',
-    element: <TravelPage />,
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+      {
+        path: 'travel-form',
+        element: <CreateFormPage />,
+      },
+      {
+        path: 'travel',
+        element: <TravelPage />,
+      },
+    ],
   },
 ]);
 export default router;
