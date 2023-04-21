038486a22c3080146bb83010558f9b75a318c40f
luis <luisrym@gmail.com>
2023-04-21 23:34:15 +0200

    delete getAllTravels and create getTravelByEmail

--- src/features/user/components/travels/travel-api.ts
+++ src/features/user/components/travels/travel-api.ts
@@ -1,6 +1,6 @@
-export const getAllTravels = async () => {
+export const getTravelsByEmailCreator = async (userEmail: string) => {
   const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/api/v1/travel`,
+    `${process.env.REACT_APP_API_URL}/api/v1/travel/${userEmail}`,
     {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
